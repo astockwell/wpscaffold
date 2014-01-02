fd603e7f8e4e3f8f433bb9613a8b54591792e296
Alex Stockwell <astockwell@gmail.com>
2014-01-01 22:51:10 -0700

    comments and cleanup

--- lib/wpscaffold/ACF/field_group.rb
+++ lib/wpscaffold/ACF/field_group.rb
@@ -18,17 +18,17 @@ module Wpscaffold
 
 			end
 
+			# Create instances of correct fieldtypes
 			def field_factory(field_name, field_type, order_no, modifiers=[], options={})
 				field_class_name = "#{field_type.to_s.capitalize}Field"
-
-				unless field_class = field_type_exists?(field_class_name)
-					raise ArgumentError, "No fieldtype (#{field_class}) for (#{field_name}) exists or is defined."
-				end
+				raise ArgumentError, "No fieldtype (#{field_class}) for (#{field_name}) exists or is defined." unless field_class = field_type_exists?(field_class_name)
+				# todo: modifiers array should be removed, it is UI specific, and modifiers should be passed as options and handled thusly by individual fieldtypes
 				field_class.new(field_name, order_no, modifiers, options)
 			end
 
 			# private
 
+			# Safely get fieldtype's class from user input
 			def field_type_exists?(field_type)
 				klass = ACF.const_get(field_type)
 				return klass.is_a?(Class) ? klass : false
